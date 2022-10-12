fc4fb85d59b12591d9895157f3f7acc46f90fdd6
Timur <vip.firemen@icloud.com>
2022-10-13 07:29:00 +0800

    Initial Commit

---
+++ MVP app practise 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP app practise 2
+//
+//  Created by Aid on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
